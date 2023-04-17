c086a9986e44436a38b26f579713becc3b754535
felipe <felipesilvasantoslawliet1@gmail.com>
2023-04-17 10:20:01 -0300

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    Highlight: "#FFBA08",
+    dark: {
+      "black": "#000000",
+      "text": "#47585B",
+      "info": "#999999",
+    },
+    light: {
+      "white": "#FFFFFF",
+      "text": "#F5F8FA",
+      "info": "#DADADA",
+    }
+  },
+  fonts: {
+    heading: `'Poppins', sans-serif`,
+    body: `'Poppins', sans-serif`,
+    card: {
+      heading: `'Barlow', sans-serif`,
+      body: `'Barlow', sans-serif`,
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'light.white',
+        color: 'dark.text'
+      }
+    }
+  }
+})
